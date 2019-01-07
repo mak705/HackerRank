baca45fa329ede5d57a369372b3860da115d1207
Hussain, Makbul <Makbul.Hussain@clarivate.com>
2019-01-07 13:23:20 +0530

    Create itertools.py

---
+++ itertools.py
@@ -0,0 +1,6 @@
+from itertools import combinations
+
+_,s,n = input(),input().split(),int(input())
+t = list(combinations(s,n))
+f = [i for i in t if 'a' in i]
+print("{0:.12}".format(len(f)/len(t)))
