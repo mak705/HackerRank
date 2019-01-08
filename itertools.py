45db71639163227d7b0d83b5f70d761e571cfa60
Makbul Hussain <mak705@gmail.com>
2019-01-08 10:34:04 +0530

    itertools with combination

--- itertools.py
+++ itertools.py
@@ -3,4 +3,4 @@ from itertools import combinations
 _,s,n = input(),input().split(),int(input())
 t = list(combinations(s,n))
 f = [i for i in t if 'a' in i]
-print("{0:.12}".format(len(f)/len(t)))
+print("{0:.12}".format(len(f)/len(t))) 
